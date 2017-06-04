368db7801f204fdc909ee68c6a5621042156056e
Carlos Martins <cmartins@stone.com.br>
2017-06-04 19:40:21 -0300

    Calculating lagrange multipliers

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 import numpy as np
 from svm import SVM
+from kernels import Kernel
 
 positives = [[5,1], [6,-1], [7,3]]
 negatives = [[1,7], [2,8], [3,8]]
 
 data_dict = {-1: np.array(negatives), 1: np.array(positives)}
 
-svm = SVM(weights=np.array([0.1, 0.2]), bias=0.1, data=data_dict)
+svm = SVM(weights=np.array([0.1, 0.2]), bias=0.1, data=data_dict, kernel=Kernel.linear())
 
-test_data = [[0,10], [1,3], [3,4], [3,5], [5,5], [5,6], [6,-5], [5,8]]
-svm.plot()
+svm.fit()
+# test_data = [[0,10], [1,3], [3,4], [3,5], [5,5], [5,6], [6,-5], [5,8]]
+#
+# svm.plot()
--- svm.py
+++ svm.py
@@ -1,11 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import cvxopt
 
 class SVM:
-    def __init__(self, bias, weights, data):
+    def __init__(self, bias, weights, data, kernel):
+        self.kernel = kernel
         self.bias = bias
         self.weights = weights
         self.data_dict = data
+        self.X = np.append(self.data_dict[1], self.data_dict[-1], axis=0)
+
+        self.Y = np.zeros(self.X.shape[0])
+        len_pos = len(self.data_dict[1])
+        len_neg = len(self.data_dict[-1])
+
+        for i in range(0, len_pos):
+            self.Y[i] = 1
+        for i in range(0, len_neg):
+            self.Y[i+len_pos] = -1
+
+    def get_lagrange_multipliers(self, X, y):
+        n_samples, n_features = X.shape
+        K = np.zeros((n_samples, n_samples))
+
+        for i, x_i in enumerate(X):
+            for j, x_j in enumerate(X):
+                K[i, j] = self.kernel(x_i, x_j)
+
+        P = cvxopt.matrix(np.outer(y, y) * K)
+        q = cvxopt.matrix(-1 * np.ones(n_samples))
+
+        G_std = cvxopt.matrix(np.diag(np.ones(n_samples) * -1))
+        h_std = cvxopt.matrix(np.zeros(n_samples))
+
+        G_slack = cvxopt.matrix(np.diag(np.ones(n_samples)))
+        h_slack = cvxopt.matrix(np.ones(n_samples) * self.bias)
+
+        G = cvxopt.matrix(np.vstack((G_std, G_slack)))
+        h = cvxopt.matrix(np.vstack((h_std, h_slack)))
+
+        A = cvxopt.matrix(y, (1, n_samples))
+        b = cvxopt.matrix(0.0)
+
+        solution = cvxopt.solvers.qp(P, q, G, h, A, b)
+
+        return np.ravel(solution['x'])
 
     def plot(self):
         colors = {1:'r',-1:'b'}
@@ -23,4 +62,4 @@ class SVM:
         return classification
 
     def fit(self):
-        pass
+        multipliers = self.get_lagrange_multipliers(self.X, self.Y)
