b48e0c382c9968a29d783af5f37e691a00b98203
Carlos Martins <cmartins@stone.com.br>
2017-06-06 23:19:08 -0300

    Fixing fit function; completing a dimension at the time of prediction

--- run.py
+++ run.py
@@ -2,15 +2,30 @@ import numpy as np
 from svm import SVM
 from kernels import Kernel
 
-positives = [[4,4], [5,5]]#[[5,1], [6,-1], [7,3]]
-negatives = [[2,1], [1,2]]#[[1,7], [2,8], [3,8]]
+positives = [[4,4], [5,5]]
+# positives = [[5,1], [6,-1], [7,3]]
+
+# positives = [[-0.23530383,  1.70585848],
+#             [ 0.8157926 ,  0.04591391],
+#             [ 0.03237168,  1.36243792]]
+
+negatives = [[2,1], [1,2]]
+# negatives = [[1,7], [2,8], [3,8]]
+
+# negatives = [[-0.07810244, -0.65502153],
+#             [ 0.25648505, -0.79438534],
+#             [-0.83531028, -0.18554141],
+#             [ 0.41896733, -0.73003242],
+#             [-1.00007796,  0.00366544],
+#             [-1.58005843,  0.83875439],
+#             [ 0.77187267, -1.67242829]]
 
 data_dict = {-1: np.array(negatives), 1: np.array(positives)}
 
-svm = SVM(weights=np.array([0.1, 0.2]), bias=0.1, data=data_dict, kernel=Kernel.linear(), c=1)
+svm = SVM(data=data_dict, kernel=Kernel.linear(), c=1)
 
 svm.fit()
-# test_data = [[0,10], [1,3], [3,4], [3,5], [5,5], [5,6], [6,-5], [5,8]]
+test_data = [[0,10], [1,3], [3,4], [3,5], [5,6], [6,-5], [5,8]]
 
-# print(svm.predict(test_data))
+print(svm.predict(test_data))
 svm.plot()
--- svm.py
+++ svm.py
@@ -3,11 +3,10 @@ import matplotlib.pyplot as plt
 import cvxopt
 
 class SVM:
-    def __init__(self, bias, weights, data, kernel, c):
+    def __init__(self, data, kernel, c):
         self._c = c
         self.kernel = kernel
-        self.bias = bias
-        self.weights = weights
+        self.bias = 0.0
         self.data_dict = data
         self.X = np.append(self.data_dict[1], self.data_dict[-1], axis=0)
 
@@ -43,22 +42,43 @@ class SVM:
         A = cvxopt.matrix(y, (1, n_samples))
         b = cvxopt.matrix(0.0)
 
+        # cvxopt.solvers['show_progress'] = False
         solution = cvxopt.solvers.qp(P, q, G, h, A, b)
 
         return np.ravel(solution['x'])
 
+    def fit(self):
+        lagrange_multipliers = self.get_lagrange_multipliers(self.X, self.Y)
+        # print(lagrange_multipliers)
+        indices = lagrange_multipliers > 1e-5
+
+        self.weights = lagrange_multipliers[indices]
+        # print(self.weights)
+        self.support_vectors = self.X[indices]
+        # print(self.support_vectors)
+        self.support_vectors_labels = self.Y[indices]
+        #print(self.support_vectors_labels)
+        results = []
+        for (y, x) in zip(self.support_vectors_labels, self.support_vectors):
+            pred = self.bias
+            for z_i, x_i, y_i in zip(self.weights, self.support_vectors, self.support_vectors_labels):
+                pred += z_i * y_i * self.kernel(x_i, x)
+            pred = np.sign(pred).item()
+            results.append(y - pred)
+
+        self.bias = np.mean(results)
+        print(self.bias)
+
     def plot(self):
-        colors = {1:'r',-1:'b'}
+        colors = {1:'b',-1:'r'}
         fig = plt.figure()
         ax = fig.add_subplot(1,1,1)
         [[ax.scatter(x[0],x[1],s=100,color=colors[i]) for x in self.data_dict[i]] for i in self.data_dict]
-        # [ax.scatter(x[0], x[1], s=100, color='k') for x in self.features]
-        # plt.show()
 
-
-        #pv_1 = (-self.weights[0]*self.support_vectors[0]-self.bias+1) / self.weights[1]
-        #pv_2 = (-self.weights[0]*self.support_vectors[0]-self.bias+1) / self.weights[1]
-        #ax.plot(self.support_vectors, [pv_1], 'k')
+        [ax.scatter(x[0],x[1],s=100,color='k') for x in self.test]
+        # pv_1 = (-self.weights[0]*self.support_vectors[0]-self.bias+1) / self.weights[1]
+        # pv_2 = (-self.weights[0]*self.support_vectors[0]-self.bias+1) / self.weights[1]
+        # ax.plot(self.support_vectors, [pv_1], 'k')
         #
         # nv_1 = (-self.weights[0]*self.support_vectors[1]-self.bias-1) / self.weights[1]
         # nv_2 = (-self.weights[0]*self.support_vectors[1]-self.bias-1) / self.weights[1]
@@ -70,32 +90,12 @@ class SVM:
 
         plt.show()
 
-    def predict(self, features):
-        self.features = np.array(features)
+    def predict(self, test_data):
+        self.test = np.array(test_data)
         classes = []
-        for feat in features:
-            dot_product = np.dot(feat, self.weights)
+        for t in self.test:
+            t = [t[0], t[1], 1]
+            dot_product = np.dot(t, self.weights)
             classes.append(np.sign(dot_product + self.bias))
 
         return classes
-
-    def fit(self):
-        lagrange_multipliers = self.get_lagrange_multipliers(self.X, self.Y)
-        indices = lagrange_multipliers > 1e-5
-        self.weights = lagrange_multipliers[indices]
-        print(self.weights)
-        self.support_vectors = self.X[indices]
-        #print(self.support_vectors)
-        self.support_vectors_labels = self.Y[indices]
-        #print(self.support_vectors_labels)
-        results = []
-        for (y, x) in zip(self.support_vectors_labels, self.support_vectors):
-            pred = 0
-            for z_i, x_i, y_i in zip(self.weights, self.support_vectors, self.support_vectors_labels):
-                pred += z_i * y_i * self.kernel(x_i, x)
-            #pred = np.sign(pred).item()
-            results.append(y - pred)
-
-        #results = np.sign(results)
-        self.bias = np.mean(results)
-        print(self.bias)
